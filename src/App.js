0285a4f94700846c0a0da4d8ca257a53aba378f9
ramanrisaanva <rrisaanva02@gmail.com>
2021-10-11 22:58:24 +0530

    second commint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from './components/NavBar';
-import InfoCollecter from './components/InfoCollecter';
+import NavBar from './NavBar';
+import InfoCollecter from './InfoCollecter';
 import PlayArea from './PlayArea';
 import {
   BrowserRouter as Router,
